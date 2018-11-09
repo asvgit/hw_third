98cce931b1cc05ce0b31825bacd4b233db2f2da9
Арляпов Савелий <sava_arl@mail.ru>
2018-11-10 02:55:45 +0800

    add simple map

--- main.cpp
+++ main.cpp
@@ -25,47 +25,48 @@ struct logging_allocator {
 
 	T *allocate(std::size_t n) {
 		if (m_mem.size() * m_max_part_size <= m_container_size) {
-			std::cout << __PRETTY_FUNCTION__ << "[n = " << m_max_part_size * n << "]" << std::endl;
+			// std::cout << __PRETTY_FUNCTION__ << "[n = " << m_max_part_size * n << "]" << std::endl;
 			auto p = std::malloc(m_max_part_size * n * sizeof(T));
 			if (!p)
 				throw std::bad_alloc();
 			m_mem.push_back(p);
 			return reinterpret_cast<T *>(p);
 		} else {
-			std::cout << __PRETTY_FUNCTION__ << "[n = " << m_max_part_size * n << "]" <<
-				"(allocated)" << std::endl;
-			auto p = m_mem.back() + (m_container_size % m_max_part_size) * sizeof(T);
+			// std::cout << __PRETTY_FUNCTION__ << "[n = " << m_max_part_size * n << "]" <<
+			//     "(allocated)" << std::endl;
+			const size_t step = (m_container_size % m_max_part_size) * sizeof(T);
+			auto p = (size_t)m_mem.back() + step;
 			return reinterpret_cast<T *>(p);
 		}
 	}
 
 	void deallocate(T *p, std::size_t n) {
 		if (!m_container_size) {
-			std::cout << __PRETTY_FUNCTION__ << "[n = " << n << "](clean all)" << std::endl;
+			// std::cout << __PRETTY_FUNCTION__ << "[n = " << n << "](clean all)" << std::endl;
 			std::free(p);
 			m_mem.clear();
 		} else {
-			std::cout << __PRETTY_FUNCTION__ << "[n = " << n << "]" <<
-				"(fictitious)" << "(" << m_container_size << "," << m_mem.size() << ")";
+			// std::cout << __PRETTY_FUNCTION__ << "[n = " << n << "]" <<
+			//     "(fictitious)" << "(" << m_container_size << "," << m_mem.size() << ")";
 			if (!(m_container_size % m_max_part_size)) {
 				std::free(reinterpret_cast<T *>(m_mem.back()));
-				std::cout << "(free last)";
+				// std::cout << "(free last)";
 				m_mem.pop_back();
 			}
-			std::cout << std::endl;
+			// std::cout << std::endl;
 		}
 	}
 
 	template<typename U, typename ...Args>
 	void construct(U *p, Args &&...args) {
-		std::cout << __PRETTY_FUNCTION__ << "(c_size: " << m_container_size << ")" << std::endl;
+		// std::cout << __PRETTY_FUNCTION__ << "(c_size: " << m_container_size << ")" << std::endl;
 		new(p) U(std::forward<Args>(args)...);
 		++m_container_size;
 	}
 
 	template<typename U>
 	void destroy(U *p) {
-		std::cout << __PRETTY_FUNCTION__ << std::endl;
+		// std::cout << __PRETTY_FUNCTION__ << std::endl;
 		p->~U();
 		--m_container_size;
 	}
@@ -91,19 +92,22 @@ struct fact<0> {
 
 constexpr int magic_num = 10;
 
+using factorial = fact<magic_num>;
+
 int main() {
 	try {
-		for (size_t i = 0; i < magic_num; ++i) {
-			std::cout << fact<magic_num>::val(i) << std::endl;
+		{
+			std::map<int, int> m{};
+			for (size_t i = 0; i < magic_num; ++i) {
+				m[i] = factorial::val(i);
+			}
 		}
-
-		auto m = std::map<int, int, std::less<int>, logging_allocator<std::pair<const int, int>, magic_num>>{};
-		for (size_t i = 0; i < 20; ++i) {
-			m[i] = i;
-			std::cout << std::endl;
-		}
-		for (size_t i = 0; i < 20; ++i) {
-			std::cout << m[i] << std::endl;
+		{
+			std::map<int, int, std::less<int>, logging_allocator<std::pair<const int, int>, magic_num / 2>> m{};
+			for (size_t i = 0; i < magic_num; ++i) {
+				m[i] = factorial::val(i);
+				std::cout << i << " " << m[i] << std::endl;
+			}
 		}
 	} catch(const std::exception &e) {
 		std::cerr << e.what() << std::endl;
