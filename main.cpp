e6ce1347e47bf72b1a12970f970007c485118c75
Арляпов Савелий <sava_arl@mail.ru>
2018-11-26 22:57:07 +0800

    Revert "wip: do cont with my alloc"
    This reverts commit 7f2f1176ddfcbe66cce965c13c30729fc8d3bd74.

--- main.cpp
+++ main.cpp
@@ -9,36 +9,26 @@ using factorial = util::fact<magic_num>;
 template<typename T, typename _Alloc = std::allocator<T>>
 class Array {
 
-	_Alloc m_alloc1;
-	_Alloc m_alloc2;
-	bool use_first_alloc;
+	_Alloc m_alloc;
 	int m_size;
 	T *m_data;
 
 public:
-	Array(): m_size(0)
-			, m_data(nullptr)
-			, m_alloc1()
-			, m_alloc2()
-			, use_first_alloc(true) {}
+	Array(): m_size(0), m_data(nullptr), m_alloc() {}
 
 	~Array() { delete[] m_data; }
 
 	void push_back(const T val) {
-		auto &alloc = use_first_alloc ? m_alloc1 : m_alloc2;
-		auto &realloc = use_first_alloc ? m_alloc2 : m_alloc1;
-
-		auto new_data = realloc.allocate(m_size + 1);
+		auto new_data = m_alloc.allocate(m_size + 1);
 		for (int i = 0; i < m_size; ++i) {
-			realloc.construct(&new_data[i], m_data[i]);
-			alloc.destroy(&m_data[i]);
+			m_alloc.construct(&new_data[i], m_data[i]);
+			m_alloc.destroy(&m_data[i]);
 		}
-		realloc.construct(&new_data[m_size], val);
+		m_alloc.construct(&new_data[m_size], val);
 		if (m_size)
-			alloc.deallocate(m_data, m_size);
+			m_alloc.deallocate(m_data, m_size);
 		m_data = new_data;
 		++m_size;
-		use_first_alloc = !use_first_alloc;
 	}
 
 	int& operator[](int index) {
@@ -51,19 +41,19 @@ public:
 
 int main() {
 	try {
-		{
-			std::map<int, int> m{};
-			for (size_t i = 0; i < magic_num; ++i) {
-				m[i] = factorial::val(i);
-			}
-		}
-		{
-			std::map<int, int, std::less<int>, logging_allocator<std::pair<const int, int>, magic_num / 2>> m{};
-			for (size_t i = 0; i < magic_num; ++i)
-				m[i] = factorial::val(i);
-			for (size_t i = 0; i < magic_num; ++i)
-				std::cout << i << " " << m[i] << std::endl;
-		}
+		// {
+		//     std::map<int, int> m{};
+		//     for (size_t i = 0; i < magic_num; ++i) {
+		//         m[i] = factorial::val(i);
+		//     }
+		// }
+		// {
+		//     std::map<int, int, std::less<int>, logging_allocator<std::pair<const int, int>, magic_num / 2>> m{};
+		//     for (size_t i = 0; i < magic_num; ++i) {
+		//         m[i] = factorial::val(i);
+		//         std::cout << i << " " << m[i] << std::endl;
+		//     }
+		// }
 
 		{
 			Array<int> a;
@@ -75,15 +65,15 @@ int main() {
 			}
 		}
 
-		{
-			Array<int, logging_allocator<int, 1>> a;
-			for (size_t i = 0; i < magic_num; ++i) {
-				a.push_back(factorial::val(i));
-			}
-			for (size_t i = 0; i < magic_num; ++i) {
-				std::cout << i << " " << a[i] << std::endl;
-			}
-		}
+		// {
+		//     Array<int, logging_allocator<int, magic_num>> a;
+		//     for (size_t i = 0; i < magic_num; ++i) {
+		//         a.push_back(factorial::val(i));
+		//     }
+		//     for (size_t i = 0; i < magic_num; ++i) {
+		//         std::cout << i << " " << a[i] << std::endl;
+		//     }
+		// }
 	} catch(const std::exception &e) {
 		std::cerr << e.what() << std::endl;
 	}
